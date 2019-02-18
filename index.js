f49c11dc4c879fc9c115dad2f2b16adf71826e49
steven waters <swaters86@gmail.com>
2019-02-18 12:02:18 -0500

    Added a new object for for software and the markup for it and styles for it so the text and ratings bar will be aligned to the right side

--- index.js
+++ index.js
@@ -3,13 +3,34 @@ const pug = require('pug');
 const fs = require('fs');
 
 const languagesObj = {
-    'HTML5':'90',
-    'CSS3':'80',
-    'ES6':'70'
+    'HTML5' : '90',
+    'CSS3' : '80',
+    'ES6' : '70', 
+    'PHP' : '80', 
+    'MySQL' : '80',
+    'Python' : '50', 
 };
 
+const softwareObj = {
+    'Adobe Illustrator' : '90', 
+    'Adobe Photoshop' : '80',
+    'Balsamiq' : '90', 
+    'Adobe Premier' : '40', 
+    'Visual Studio Code' : '70', 
+    'Eclipse' : '80',
+}
+
+const toolsObj = {
+    'GIT' : '60',
+    'SVN' : '60',
+    'Webpack' : '50', 
+    'Gulp.js' : '50',  
+}
+
 let html = pug.renderFile('templates/index.pug', {
-    languages: languagesObj
+    languages: languagesObj, 
+    software: softwareObj,
+    tools: toolsObj,
 });
 
 fs.writeFile("test.html", html, function(err) {
