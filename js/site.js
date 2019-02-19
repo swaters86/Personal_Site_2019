b47528d4a124dec4aa72a7f0aec1d1d035cc99cb
steven waters <swaters86@gmail.com>
2019-02-19 02:24:01 -0500

    Cleaned up selector names and mark up. Added docblock to site.css stylesheet, reogranized property names in site.css stylesheet

--- js/site.js
+++ js/site.js
@@ -1,14 +1,14 @@
 'use strict';
 
 (function () {
-    var pillTabContentContainers = document.querySelectorAll('.pill__tabs__nav__item__content');
-    var pillTabLinks = document.querySelectorAll('.pill__tabs__nav__item__link');
+    var pillTabContentContainers = document.querySelectorAll('.tablist__tab__content');
+    var pillTabLinks = document.querySelectorAll('.tablist__tab__link');
 
     // Hide all links and tabs except the ones with an index value of 0
     pillTabContentContainers.forEach(function (pillTabContent) {
-        if (pillTabContent.id !== 'tab__content-0') {
+        if (pillTabContent.id !== 'tab-0') {
             pillTabContent.style.display = 'none';
-            pillTabContent.classList = 'pill__tabs__nav__item__content';
+            pillTabContent.classList = 'tablist__tab__content';
         }
     });
 
@@ -16,26 +16,28 @@
     function updateTabStates(pillTabContentContainers) {
         pillTabContentContainers.forEach(function (pillTabContent) {
             pillTabContent.style.display = 'none';
-            pillTabContent.classList = 'pill__tabs__nav__item__content';
+            pillTabContent.classList = 'tablist__tab__content';
         });
 
         pillTabLinks.forEach(function (link) {
-            link.classList = 'pill__tabs__nav__item__link';
+            link.classList = 'tablist__tab__link';
         });
     }
 
-    var links = document.querySelectorAll('.pill__tabs__nav__item__link');
+    var links = document.querySelectorAll('.tablist__tab__link');
     links.forEach(function (link) {
-        link.addEventListener('click', function () {
+        link.addEventListener('click', function (e) {
+            e.preventDefault();
+
             var tabNumber = this.hash.substr(1).slice(-1);
-            var tabContent = document.getElementById('tab__content-' + tabNumber);
+            var tabContent = document.getElementById('tab-' + tabNumber);
 
             if (this.click) {
                 updateTabStates(pillTabContentContainers);
 
                 tabContent.style.display = 'block';
-                tabContent.classList = 'pill__tabs__nav__item__content pill__tabs__nav__item__content--active';
-                this.classList = 'pill__tabs__nav__item__link pill__tabs__nav__item__link--active';
+                tabContent.classList = 'tablist__tab__content tablist__tab__content--active';
+                this.classList = 'tablist__tab__link tablist__tab__link--active';
             }
         });
     });
