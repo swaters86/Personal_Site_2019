f967fecdb6057da0a4aa29ff20217e42e9f771a8
steven waters <swaters86@gmail.com>
2019-02-20 14:40:06 -0500

    Removed code for sticky tabs and added a projects section with a crude hover effect for each portoflio thumbnail

--- index.js
+++ index.js
@@ -2,56 +2,62 @@ const pug = require('pug');
 const fs = require('fs');
 
 const languagesObj = {
-    'HTML5' : '90',
-    'CSS3' : '80',
-    'ES6' : '70',
-    'PHP' : '80',
-    'MySQL' : '80',
-    'Python' : '50',
-    'Node.js' : '60'
+    'HTML5': '90',
+    'CSS3': '80',
+    'ES6': '70',
+    'PHP': '80',
+    'MySQL': '80',
+    'Python': '50',
+    'Node.js': '60',
+    'JSON': '90',
+    'XML': '95',
 };
 
 const softwareObj = {
-    'Adobe Illustrator' : '90',
-    'Adobe Photoshop' : '80',
-    'Balsamiq' : '90',
-    'Adobe Premier' : '40',
-    'Visual Studio Code' : '70',
-    'Eclipse' : '80',
+    'Adobe Illustrator': '90',
+    'Adobe Photoshop': '80',
+    'Balsamiq': '90',
+    'Adobe Premier': '40',
+    'Visual Studio Code': '70',
+    'Eclipse': '80',
 }
 
 const toolsObj = {
-    'GIT' : '60',
-    'SVN' : '60',
-    'Webpack' : '50',
-    'Gulp.js' : '50',
+    'GIT': '60',
+    'SVN': '60',
+    'Webpack': '50',
+    'Gulp.js': '50',
+    'NPM': '60',
+    'Chrome Developer Tools': '70',
+    'Firefox Developer Tools': '60',
 }
 
 const frameworksObj = {
-    'Vue.js' : '40',
-    'Yii 1.1' : '90',
-    'Yii 2' : '80',
-    'Wordpress' : '70',
-    'Express.js' : '50'
+    'Vue.js': '40',
+    'Yii 1.1': '90',
+    'Yii 2': '80',
+    'Wordpress': '70',
+    'Express.js': '50'
 }
 
 const librariesObj = {
-    'React' : '50',
-    'Pug' : '70',
+    'React': '50',
+    'Pug': '70',
 }
 
 const otherObj = {
-    'Documentation' : '90',
-    'Graphic Design' : '90',
-    'Organization' : '80',
-    'Attention to Detail' : '100',
+    'Documentation': '90',
+    'Graphic Design': '90',
+    'Organization': '80',
+    'Attention to Detail': '90',
+    'QA': '100',
 }
 
 const skillsObj = {
-    languages: languagesObj, 
+    languages: languagesObj,
     software: softwareObj,
     tools: toolsObj,
-    frameworks: frameworksObj, 
+    frameworks: frameworksObj,
     libraries: librariesObj,
     other: otherObj,
 }
@@ -60,8 +66,8 @@ let html = pug.renderFile('templates/index.pug', {
     skills: skillsObj
 });
 
-fs.writeFile("site/index.html", html, function(err) {
-    if(err) {
+fs.writeFile("site/index.html", html, function (err) {
+    if (err) {
         return console.log(err);
     }
 
