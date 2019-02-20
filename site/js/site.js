d3f5531a08530249e70d0b6fab57d619dc7c572b
steven waters <swaters86@gmail.com>
2019-02-19 22:06:06 -0500

    Fixed issues with the sticky nav code and refactored it. Also added for a check so the sticky nav is not used for displays with a width of 320 and below

--- site/js/site.js
+++ site/js/site.js
@@ -1,60 +1,70 @@
 'use strict';
 
 (function () {
-    window.onscroll = function() {makeSticky()};
+    var viewportWidth = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+    var viewportHeight = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
 
-    var tabList = document.querySelector('.tablist');
-    var tabListContent = document.querySelector('.tablist__tab__content--active');
-    var sticky = tabList.offsetTop;
+    if (viewportWidth >= 375) {
+        window.onscroll = function () { makeSticky() };
 
-    function makeSticky() {
-        if (window.pageYOffset > sticky) {
-            tabList.classList.add('tablist--sticky');
-            tabListContent.classList.add('tablist__content--sticky');
-        } else {
-            tabList.classList.remove('tablist--sticky');
-            tabListContent.classList.remove('tablist__content--sticky');
+        var tabList = document.querySelector('.tablist');
+        var tabListContent = document.querySelector('.tablist__tab__content--active');
+        var sticky = tabList.offsetTop;
+
+
+        function makeSticky() {
+            if (window.pageYOffset > sticky) {
+                tabList.classList.add('tablist--sticky');
+                tabListContent.classList.add('tablist__content--sticky');
+            } else {
+                tabList.classList.remove('tablist--sticky');
+                tabListContent.classList.remove('tablist__content--sticky');
+                tabListContent.querySelector('.skills__headline__level__2').classList.remove('skills__headline__level__2--sticky');
+            }
         }
-    }
 
-    var pillTabContentContainers = document.querySelectorAll('.tablist__tab__content');
-    var pillTabLinks = document.querySelectorAll('.tablist__tab__link');
+        var pillTabContentContainers = document.querySelectorAll('.tablist__tab__content');
+        var pillTabLinks = document.querySelectorAll('.tablist__tab__link');
 
-    // Hide all links and tabs except the ones with an index value of 0
-    pillTabContentContainers.forEach(function (pillTabContent) {
-        if (pillTabContent.id !== 'tab-0') {
-            pillTabContent.style.display = 'none';
-            pillTabContent.classList = 'tablist__tab__content';
-        }
-    });
-
-    // Hide all links and tabs except the ones that are marked as active
-    function updateTabStates(pillTabContentContainers) {
+        // Hide all links and tabs except the ones with an index value of 0
         pillTabContentContainers.forEach(function (pillTabContent) {
-            pillTabContent.style.display = 'none';
-            pillTabContent.classList = 'tablist__tab__content';
-        });
-
-        pillTabLinks.forEach(function (link) {
-            link.classList = 'tablist__tab__link';
-        });
-    }
-
-    var links = document.querySelectorAll('.tablist__tab__link');
-    links.forEach(function (link) {
-        link.addEventListener('click', function (e) {
-            e.preventDefault();
-
-            var tabNumber = this.hash.substr(1).slice(-1);
-            var tabContent = document.getElementById('tab-' + tabNumber);
-
-            if (this.click) {
-                updateTabStates(pillTabContentContainers);
-
-                tabContent.style.display = 'block';
-                tabContent.classList = 'tablist__tab__content tablist__tab__content--active';
-                this.classList = 'tablist__tab__link tablist__tab__link--active';
+            if (pillTabContent.id !== 'tab-0') {
+                pillTabContent.style.display = 'none';
+                pillTabContent.classList = 'tablist__tab__content';
             }
         });
-    });
+
+        // Hide all links and tabs except the ones that are marked as active
+        function updateTabStates(pillTabContentContainers) {
+            pillTabContentContainers.forEach(function (pillTabContent) {
+                pillTabContent.style.display = 'none';
+                pillTabContent.classList = 'tablist__tab__content';
+            });
+
+            pillTabLinks.forEach(function (link) {
+                link.classList = 'tablist__tab__link';
+            });
+        }
+
+        var links = document.querySelectorAll('.tablist__tab__link');
+        links.forEach(function (link) {
+            link.addEventListener('click', function (e) {
+                e.preventDefault();
+
+                var tabNumber = this.hash.substr(1).slice(-1);
+                var tabContent = document.getElementById('tab-' + tabNumber);
+
+                if (this.click) {
+                    updateTabStates(pillTabContentContainers);
+                    tabContent.style.display = 'block';
+                    tabContent.classList = 'tablist__tab__content tablist__tab__content--active';
+                    this.classList = 'tablist__tab__link tablist__tab__link--active';
+                    
+                    if (window.scrollY > sticky) {
+                        window.scroll(0, 605);
+                    }
+                }
+            });
+        });
+    }
 })();
